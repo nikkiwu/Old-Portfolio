0893c83d046c974adf4e06d6cdf755b35976d087
Nikki Wu <nikkiwu9@gmail.com>
2019-03-16 15:12:32 -0400

    add photo section

--- js/script.js
+++ js/script.js
@@ -42,6 +42,41 @@ $(document).ready(function () {
             }
         });
 
+    // Show all
+    $('.filter-all').on('click', function () {
+        var $this = $(this);
+        $('.filter').removeClass('active');
+        $this.addClass('active');
+        $('.all').removeClass('hide');
+    });
+
+// Show landscape
+    $('.filter-landscape').on('click', function () {
+        var $this = $(this);
+        $('.filter').removeClass('active');
+        $this.addClass('active');
+        $('.landscape').removeClass('hide');
+        $('.urban, .portrait').addClass('hide');
+    });
+
+// Show urban
+    $('.filter-urban').on('click', function () {
+        var $this = $(this);
+        $('.filter').removeClass('active');
+        $this.addClass('active');
+        $('.urban').removeClass('hide');
+        $('.portrait, .landscape').addClass('hide');
+    });
+
+// Show portrait
+    $('.filter-portrait').on('click', function () {
+        var $this = $(this);
+        $('.filter').removeClass('active');
+        $this.addClass('active');
+        $('.portrait').removeClass('hide');
+        $('.landscape, .urban').addClass('hide');
+    });
+
 
 });
 
